b25069f01f4a35d12cbefb1f632013c9a39262aa
Marouane <57333995+riimuru@users.noreply.github.com>
2022-07-22 23:25:47 -0400

    feat (frontend-mobile): action-bar with broken drawer (#3)

--- frontend/mobile/App.jsx
+++ frontend/mobile/App.jsx
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import Home from './src/Home';
+import Home from './src/screens/Home';
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Home />
-    </NavigationContainer>
-  );
+ return (
+  <NavigationContainer>
+   <Home />
+  </NavigationContainer>
+ );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
+ },
 });
