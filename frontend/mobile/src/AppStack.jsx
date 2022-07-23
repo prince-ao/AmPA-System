db26840ab576779ce33f5044fe07279237dd1edc
prince-ao <princead@tutanota.com>
2022-07-23 12:34:48 -0400

    feat: finished animal control officer

--- frontend/mobile/src/AppStack.jsx
+++ frontend/mobile/src/AppStack.jsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Home, PetDB, PetView, MPetReq } from './screens';
+import { Home, PetDB, PetView, MPetReq, ACO, PetEntry, Camera } from './screens';
 
 const Stack = createStackNavigator();
 
@@ -11,11 +11,15 @@ const AppStack = () => {
    screenOptions={{
     headerShown: false,
    }}
+   initialRouteName="Animal Control Officer"
   >
    <Stack.Screen name="Home" component={Home} />
+   <Stack.Screen name="Animal Control Officer" component={ACO} />
    <Stack.Screen name="Pet Database" component={PetDB} />
    <Stack.Screen name="Make Pet Request" component={MPetReq} />
    <Stack.Screen name="Pet View" component={PetView} />
+   <Stack.Screen name="Pet Entry" component={PetEntry} />
+   <Stack.Screen name="Camera" component={Camera} />
   </Stack.Navigator>
  );
 };
--- frontend/mobile/src/screens/index.js
+++ frontend/mobile/src/screens/index.js
@@ -2,5 +2,8 @@ import Home from './Home';
 import PetDB from './petDB';
 import PetView from './PetView';
 import MPetReq from './MPetReq';
+import ACO from './ACO';
+import PetEntry from './PetEntry';
+import Camera from './Camera';
 
-export { Home, PetDB, PetView, MPetReq };
+export { Home, PetDB, PetView, MPetReq, ACO, PetEntry, Camera };
